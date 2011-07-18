d195e655bbb6aa9815ff37a249a974f489da9235
prince <prince@prince-VirtualBox.(none)>
2011-07-18 15:24:58 +0530

    changed credentials

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	echo "We are at root."
-mysql_connect("localhost:/tmp/mysql/tinisha.sock", "jenna", "xMGN0ZOl") or die(mysql_error());
+	echo "We are at root.";
+	mysql_connect("localhost:/tmp/mysql/tinisha.sock", "jenna", "xMGN0ZOl") or die(mysql_error());
 echo "Connected to MySQL<br />";
 ?>
